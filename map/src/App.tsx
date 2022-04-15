3d4f3eaaae3389e21b788d576e6effc407a336f2
Alex Dikelsky <alexander.dikelsky@gmail.com>
2022-04-15 14:08:08 -0500

    Start using konva, which has nice declarative canvas drawing capabilities

--- map/src/App.tsx
+++ map/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Stage, Layer, Rect, Text, Circle, Line } from 'react-konva';
 
 interface Location {
     x: number
@@ -29,27 +30,30 @@ interface Row {
 
 class Grid extends React.Component {
     data: Array<Row>;
-
+    constructor(params: any) {
+	super(params);
+	this.data = [];
+    }
 }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> asdf save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Circle x={200} y={100} radius={50} fill="green" />
+        <Line
+          x={20}
+          y={200}
+          points={[0, 0, 100, 0, 100, 100]}
+          tension={0.5}
+          closed
+          stroke="black"
+          fillLinearGradientStartPoint={{ x: -50, y: -50 }}
+          fillLinearGradientEndPoint={{ x: 50, y: 50 }}
+          fillLinearGradientColorStops={[0, 'red', 1, 'yellow']}
+        />
+      </Layer>
+    </Stage>
   );
 }
 
